7bb3508bd59271a57cc0ed6e657906fa0788f833
Eilon Shimony <eilonshi@gmail.com>
2021-06-17 11:22:31 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="fast-segmentation",
-    version='0.0.4.7',
+    version='0.0.4.8',
     author="Eilon Shimony",
     author_email="eilonshi@gmail.com",
     description="A package for fast segmentation algorithm",
